ed803dc9d0d1029dc490b39458b92267a4ce07d2
zhouxuecheng2016 <zhouxuecheng2016@gmail>
2017-03-05 08:52:40 +0800

    first comit

---
+++ src/main/java/com.vip.trade/Test.java
@@ -0,0 +1,12 @@
+package com.vip.trade;
+
+/**
+ * Created by hadoop on 2016/12/11.
+ */
+public class Test {
+
+    public static void main(String args[]) {
+        System.out.println("Test");
+    }
+
+}
