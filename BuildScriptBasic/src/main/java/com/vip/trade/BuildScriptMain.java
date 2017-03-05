52fa8417e43915059839832d6037ee8dc1fff67a
zhouxuecheng2016 <zhouxuecheng2016@gmail>
2017-03-05 21:59:27 +0800

    add build script basic

---
+++ BuildScriptBasic/src/main/java/com/vip/trade/BuildScriptMain.java
@@ -0,0 +1,14 @@
+package com.vip.trade;
+
+/**
+ * Created by hadoop on 2017/3/5.
+ */
+public class BuildScriptMain {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("111");
+        Thread.sleep(5000);
+        System.out.println("222");
+    }
+
+}
