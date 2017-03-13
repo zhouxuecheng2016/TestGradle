03598a87bc395158c168976dad91f2b6e8f9ad6f
zhouxuecheng2016 <zhouxuecheng2016@gmail>
2017-03-13 22:31:58 +0800

    add build script basic

---
+++ DependencyManagementBasics/src/main/java/com/vip/trade/DependencyMain.java
@@ -0,0 +1,10 @@
+package com.vip.trade;
+
+/**
+ * Created by hadoop on 2017/3/13.
+ */
+public class DependencyMain {
+    public static void main(String[] args) {
+        System.out.println("dependency main");
+    }
+}
---
+++ QuickStart/src/main/java/com/vip/trade/QuickStartMain.java
@@ -0,0 +1,13 @@
+package com.vip.trade;
+
+/**
+ * Created by hadoop on 2017/3/9.
+ */
+public class QuickStartMain {
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
