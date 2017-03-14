e8435544eb60e372c5428806eddea49badc6f1a3
zhouxuecheng2016 <zhouxuecheng2016@gmail>
2017-03-15 07:30:10 +0800

    add build script basic

---
+++ TestWeb/src/main/java/com/vip/trade/TestWebMain.java
@@ -0,0 +1,12 @@
+package com.vip.trade;
+
+/**
+ * Created by hadoop on 2017/3/14.
+ */
+public class TestWebMain {
+
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+
+}
